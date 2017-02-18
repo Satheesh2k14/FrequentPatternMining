eabe08848bc9e172fd6362762d4d7f840eea7478
satheesh2k14 <satheesh.prabhakaran97@gmail.com>
2017-02-18 22:37:59 +0530

    Imported database

---
+++ frequent_pattern.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 22:30:55 2017
+
+@author: Satheesh
+"""
+import numpy as np
+import pandas as pd
+
+database_animals = pd.read_csv('database.csv')
+
+print(database_animals)
