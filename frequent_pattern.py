e51f54074b003734239c98c2e378259ccb1895fb
satheesh2k14 <satheesh.prabhakaran97@gmail.com>
2017-02-19 07:34:42 +0530

    unique elements identified

--- frequent_pattern.py
+++ frequent_pattern.py
@@ -7,6 +7,9 @@ Created on Sat Feb 18 22:30:55 2017
 import numpy as np
 import pandas as pd
 
-database_animals = pd.read_csv('database.csv')
+database_animals = pd.read_csv('database.csv', header =0)
 
-print(database_animals)
+#Count the number of unique instances first
+df_itemlist = database_animals['ITEM'].unique()
+
+print("The number of unique elements is " + str(len(df_itemlist)))
