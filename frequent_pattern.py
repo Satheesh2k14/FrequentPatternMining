9b4c9e7f0bd0da27f06daa18826cffc1e9d4f600
satheesh2k14 <satheesh.prabhakaran97@gmail.com>
2017-02-19 10:57:03 +0530

    created dictionary

--- frequent_pattern.py
+++ frequent_pattern.py
@@ -8,6 +8,16 @@ import numpy as np
 import pandas as pd
 
 database_animals = pd.read_csv('database.csv', header =0)
+#create a dictionary for counting frequency
+transaction_dictionary = dict()
+for i in database_animals.itertuples():
+    if i[1] in transaction_dictionary:
+        transaction_dictionary[i[1]].append(i[2])
+    else:
+        transaction_dictionary[i[1]] = [i[2]]
+
+print(transaction_dictionary)
+print(type(transaction_dictionary))
 
 #Count the number of unique instances first
 itemlist = pd.Series(database_animals['ITEM'].unique())
@@ -31,10 +41,13 @@ print(min_support_pass_list)
 
 #convert 1 item set to two item set
 itemlist_2 = []
-for i in itemlist:
-    for j in itemlist:
-        if i != j:
+for i in min_support_pass_list:
+    for j in min_support_pass_list:
+        if i != j and min_support_pass_list[min_support_pass_list == i].index[0] < min_support_pass_list[min_support_pass_list== j].index[0]:
             itemlist_2.append([i,j])
             
 print(itemlist_2)
+
+#to find frequency of the 2 itemlist
+frequency_2_item_list = pd.Series([0] * len(itemlist_2))
 
