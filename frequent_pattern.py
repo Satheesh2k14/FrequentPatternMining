e76fbe51c954ca6a4b9978c40d0d07c3400b94b7
V Shanmuga Velayutham <vshanmugavelayutham@gmail.com>
2017-02-21 06:00:36 +0530

    Update frequent_pattern.py

--- frequent_pattern.py
+++ frequent_pattern.py
@@ -7,10 +7,11 @@ Created on Sat Feb 18 22:30:55 2017
 import numpy as np
 import pandas as pd
 
-database_animals = pd.read_csv('database.csv', header =0)
+database_animals = pd.read_csv('C:/Users/V.Shanvel/Downloads/database.csv', header =0)
 
 #create a dictionary for counting frequency
 transaction_dictionary = dict()
+print(transaction_dictionary)
 for i in database_animals.itertuples():
     if i[1] in transaction_dictionary:
         transaction_dictionary[i[1]].append(i[2])
@@ -30,6 +31,7 @@ frequency_1_item_list = pd.Series([0] * len(itemlist))
 
 for i in database_animals['ITEM']:
     frequency_1_item_list[itemlist[itemlist == i].index[0]] += 1
+print(frequency_1_item_list)
 
 #Define minimum support and filter elements less than min support from itemlist_1
 minimum_support = 2
@@ -64,4 +66,35 @@ print(frequency_2_item_list)
 itemlist_2 = itemlist_2[frequency_2_item_list >= 2]
 print(itemlist_2)
 print(type(itemlist_2))
-       
+
+#convert two itemlist to 3itemlist
+itemlist_3 = []
+for i in itemlist_2:
+   for j in  itemlist_2:
+       if i[1]!=j[0] and i[1]!=j[1]:
+           itemlist_3.append([i[1],j[0],j[1]])
+       if i[0]!=j[0] and i[0]!=j[1]:
+           itemlist_3.append([i[0],j[0],j[1]])
+itemlist_3.sort()  
+print (itemlist_3)
+print ("3itemlist:")
+newlist_3 = []  
+for i in itemlist_3:
+    if i not in newlist_3:
+        newlist_3.append(i)
+
+print(newlist_3)           
+
+#to find frequency of 3 itemlist
+frequency_3_item_list = pd.Series([0] * len(newlist_3))
+
+for index,i in enumerate(newlist_3):
+    for key,value in transaction_dictionary.items():
+        if i[0] in value and i[1] in value and i[2] in value:
+            frequency_3_item_list[index] +=1
+                                 
+print(frequency_3_item_list)   
+
+#filter 3itemlist with min support
+newlist_3 = newlist_3[frequency_3_item_list >=2 ]
+print(newlist_3)      
